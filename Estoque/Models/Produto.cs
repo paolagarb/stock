823d6d7d06ef2ac73e1fd938ba174acf0352e871
Paola Garbato <paolagarbato@gmail.com>
2021-01-11 13:27:40 -0300

    Criando model - classe produto

---
+++ Estoque/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Insira o nome do produto")]
+        public string Nome { get; set; }
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage ="Insira o valor do produto")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Column(TypeName = "decimal(20,2)")]
+        [Display(Name ="Preço")]
+        public double Preco { get; set; }
+        [Required(ErrorMessage = "Insira a quantidade")]
+        public int Quantidade { get; set; }
+        public Produto() { }
+    }
+}
