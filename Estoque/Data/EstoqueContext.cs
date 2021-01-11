6b0fb26be92e2fbd1b39fa8e2bebd09774024886
Paola Garbato <paolagarbato@gmail.com>
2021-01-11 14:44:04 -0300

    Controller e View dos produtos e criação do bd com entity framework - first migration

---
+++ Estoque/Data/EstoqueContext.cs
@@ -0,0 +1,15 @@
+﻿using Estoque.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estoque.Data
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext (DbContextOptions<EstoqueContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ Estoque/Migrations/20210111174223_First.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estoque.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(20,2)", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
