a0fc3e2762a23a71ccbb216d1910bbed0e81c3f8
Paola Garbato <paolagarbato@gmail.com>
2021-01-12 19:43:57 -0300

    Criando web API

---
+++ WebApiEstoque/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace Estoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public Produto() { }
+    }
+}
